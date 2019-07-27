37a591b223871d5ec875e97129bb8d24f79dbf93
Vladimir Anisimov <wiselion@yandex.ru>
2019-07-27 22:36:21 +0300

    Добавлены заявки и шаблоны подробнее

--- www/js/lib.js
+++ www/js/lib.js
@@ -25,19 +25,25 @@ var g_templates = [];
 function LoadTemplates(prms) {
     if (prms === undefined) var prms = {};
     if(prms.func_before!==undefined) prms.func_before();
-
+console.log('load templates');
     // test ///////////////////
-    g_orders = [];
-    for(let i = 30;i>0;i--) {
-        g_orders.push(
-            {
-                id: i,
-                date: i+'.06.2019 20:'+(i+10),
-                type: i%2 ? 'Заведение заявки в Jira' : 'Заведение заявки в Bitrix',
-                status: Math.floor((30-i)/10),
-            }
-        );
-    }
+    g_templates = [
+        {
+            id: 1,
+            name: 'Заведение заявки в JIRA',
+            status: 2,
+        },
+        {
+            id: 2,
+            name: 'Заведение заявки в Bitrix',
+            status: 2,
+        },
+        {
+            id: 3,
+            name: 'Заведение контрагента',
+            status: 1,
+        },
+    ];
     // test ///////////////////
 
     if(prms.func_after!==undefined) prms.func_after();
